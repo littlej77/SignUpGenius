c4ca8c82b4b768b0a5cc3f1a9b48c468f36a3319
laurenstolk <laurenstolk@outlook.com>
2022-03-23 15:07:10 -0600

    unseed

---
+++ Migrations/20220323210244_first.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SignUpGenius.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TimeSlots",
+                columns: table => new
+                {
+                    TimeSlotID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TourTime = table.Column<DateTime>(nullable: false),
+                    Taken = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimeSlots", x => x.TimeSlotID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FormResponses",
+                columns: table => new
+                {
+                    TourId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GroupName = table.Column<string>(nullable: false),
+                    GroupSize = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Phone = table.Column<string>(nullable: true),
+                    TimeSlotID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormResponses", x => x.TourId);
+                    table.ForeignKey(
+                        name: "FK_FormResponses_TimeSlots_TimeSlotID",
+                        column: x => x.TimeSlotID,
+                        principalTable: "TimeSlots",
+                        principalColumn: "TimeSlotID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormResponses_TimeSlotID",
+                table: "FormResponses",
+                column: "TimeSlotID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FormResponses");
+
+            migrationBuilder.DropTable(
+                name: "TimeSlots");
+        }
+    }
+}
--- Models/SignUpGeniusContext.cs
+++ Models/SignUpGeniusContext.cs
@@ -20,24 +20,24 @@ namespace SignUpGenius.Models
         public DbSet<FormResponse> FormResponses { get; set; }
         public DbSet<TimeSlot> TimeSlots { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder mb)
-        {
-            mb.Entity<TimeSlot>().HasData(
-                new TimeSlot { TimeSlotID = 1, TourTime = Convert.ToDateTime(8) },
-                new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(9) },
-                new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(10) },
-                new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(11) },
-                new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(12) },
-                new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(13) },
-                new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(14) },
-                new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(15) },
-                new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(16) },
-                new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(17) },
-                new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(18) },
-                new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(19) }
+        //protected override void OnModelCreating(ModelBuilder mb)
+        //{
+        //    mb.Entity<TimeSlot>().HasData(
+        //        new TimeSlot { TimeSlotID = 1, TourTime = Convert.ToDateTime(8) },
+        //        new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(9) },
+        //        new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(10) },
+        //        new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(11) },
+        //        new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(12) },
+        //        new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(13) },
+        //        new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(14) },
+        //        new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(15) },
+        //        new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(16) },
+        //        new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(17) },
+        //        new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(18) },
+        //        new TimeSlot { TimeSlotID = 2, TourTime = Convert.ToDateTime(19) }
 
-                );
-        }
+        //        );
+        //}
 
     }
 }
